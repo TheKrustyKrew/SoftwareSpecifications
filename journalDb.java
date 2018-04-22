7668ebbd167ea73ef31f91b40545a57723c91bb5
littlepinkangel17 <resnyder2333@eagle.fgcu.edu>
2018-04-21 21:55:31 -0400

    Add files via upload

--- journalDb.java
+++ journalDb.java
@@ -10,7 +10,9 @@ public class journalDb {
 	private static final String X_VALUE_COLUMN = "X-Coordinate";
 	private static final String Y_VALUE_COLUMN = "Y-Coordinate";
 	private static final String LENGTH_COLUMN = "Length";
+	private static final String PRIVACY_COLUMN = "Privacy Setting"; 
 	private static final String SPECIES_COLUMN = "Species";
+	private static final String IMAGE_COLUMN = "Image";
 	private static final String BAIT_COLUMN = "Bait";
 	private static final String WIND_VALUE_COLUMN = "Wind";
 	private static final String TIDE_VALUE_COLUMN = "Tide";
@@ -21,8 +23,8 @@ public class journalDb {
 			"CREATE TABLE " + TABLE_NAME + "(" + USERNAME_COLUMN + " TEXT PRIMARY KEY NOT NULL," 
 			+ MONTH_COLUMN + " TEXT," + DATE_COLUMN + " TEXT," + YEAR_COLUMN + " TEXT," 
 			+ X_VALUE_COLUMN + " REAL," + Y_VALUE_COLUMN + " REAL," + LENGTH_COLUMN + " REAL," 
-			+ SPECIES_COLUMN + " TEXT," + BAIT_COLUMN + " TEXT," + WIND_VALUE_COLUMN + " TEXT," +  TIDE_VALUE_COLUMN + " Text," 
-			+ NOTES_COLUMN + " TEXT)";
+			+ PRIVACY_COLUMN + " INT," + SPECIES_COLUMN + " TEXT," + BAIT_COLUMN + " TEXT," + 
+			WIND_VALUE_COLUMN + " TEXT," +  TIDE_VALUE_COLUMN + " Text," + NOTES_COLUMN + " TEXT)";
 	
 	private static final String DELETE_ENTRIES = "DROP TABLE IF EXISTS" + TABLE_NAME;
 	
@@ -38,7 +40,9 @@ public class journalDb {
 		values.put(X_VALUE_COLUMN, CreateEntry.xcoValue);
 		values.put(Y_VALUE_COLUMN, CreateEntry.ycoValue);
 		values.put(LENGTH_COLUMN, CreateEntry.lengthValue);
+		values.put(PRIVACY_COLUMN, CreateEntry.privacy);
 		values.put(SPECIES_COLUMN, CreateEntry.speciesValue);
+		values.put(IMAGE_COLUMN, CreateEntry.myImage);
 		values.put(BAIT_COLUMN, CreateEntry.baitValue);
 		values.put(WIND_VALUE_COLUMN, CreateEntry.windValue);
 		values.put(TIDE_VALUE_COLUMN, CreateEntry.tideValue);
@@ -58,7 +62,9 @@ public class journalDb {
 				X_VALUE_COLUMN,
 				Y_VALUE_COLUMN,
 				LENGTH_COLUMN,
+				PRIVACY_COLUMN,
 				SPECIES_COLUMN,
+				IMAGE_COLUMN,
 				BAIT_COLUMN,
 				WIND_VALUE_COLUMN,
 				TIDE_VALUE_COLUMN,
